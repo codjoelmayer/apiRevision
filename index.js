c8aff7fa3c7600c42bb768cbf2be315c3a227d13
codjoelmayer <tmjoelm4@gmail.com>
2024-02-09 12:14:11 +0200

    Updates

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-import {addition} from './addition.js'
-import {subtraction} from './subtraction.js'
-
-console.log(addition(3, 6));
-console.log(subtraction(7, 3));
-console.log(subtraction(3, 7));
-/*
-The purpose of this challenge is for you to learn from each other.
-- Only two people per group
-- One person needs to create a repo and backend branch.
-- Add your partner as a collaborator.
-- Choose your idea to focus on what we covered thus far until the http module.
-- Push your task 
-- Once both partners are done with their tasks, you can create a pull request and merge them to the backend branch.
-- Ask your partner to pull the changes from the backend branch.
-- Be ready to present your work to the classes 
-======= 
-*/
+import http from 'http'
+const port = +process.env.PORT || 4000
+// Create web server
+http.createServer((req, res)=>{
+    res.writeHead( 200, {
+        'Content-type': 'application/json'
+    })
+    switch(req.url) {
+        case '/':
+            res.end(JSON.stringify("You're home"))
+        break 
+        case '/about':
+            res.end(JSON.stringify("About page"))
+        break 
+        default: 
+            res.end(JSON.stringify("404 page"))
+    }
+}).listen(port, ()=>{
+    console.log(`server is running on : http://localhost:${port}`);
+})
